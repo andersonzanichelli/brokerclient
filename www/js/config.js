4cf942c65c9faca22391cde4c12627f032c3f240
Anderson Zanichelli <anderzanic@gmail.com>
2015-04-26 18:26:36 -0300

    Criada tela de configuração.

--- www/js/config.js
+++ www/js/config.js
@@ -1,20 +1,62 @@
 angular.module('config', [])
-    .controller('configCtrl', function($scope) {
+    .controller('configCtrl', function($scope, configService) {
         $scope.save = function() {
-                var config = {
-                    address: $scope.address,
-                    port:    $scope.port
-                };
-                window.localStorage.setItem('configuration', JSON.stringify(config));
+            var config = {
+                address: $scope.address,
+                port:    $scope.port
             };
+            configService.save(config);
+            $scope.load();
+        };
 
         $scope.load = function() {
-                $scope.data = JSON.parse(window.localStorage.getItem('configuration'));
-            };
+            $scope.host = configService.load();
+        };
+
+        $scope.load();
     })
     .directive('configuration', function(){
         return {
             restrict: 'E',
             templateUrl: 'view/config/configuration.html'
+        };
+    })
+    .service('configService', function(){
+        var save= function(config){
+            window.localStorage.setItem('brokerconfig', JSON.stringify(config));
+        };
+
+        var load = function() {
+            var host = '';
+            var config = JSON.parse(window.localStorage.getItem('brokerconfig'));
+
+            if(config !== null && config.address !== '') {
+                if(temHttp(config.address))
+                    host = config.address;
+                else {
+                    var number = parseInt(config.address.substring(0,1));
+                    if(isNaN(number)) {
+                        host = 'http://' + config.address;
+                    } else {
+                        host = config.address;
+                    }
+                }
+
+                if(config.port) {
+                    if(config.port !== undefined && config.port > 0)
+                        host = host + ":" + config.port;
+                }
+            }
+
+            return host;
+        };
+
+        function temHttp(address){
+            return address.substring(0,7) === 'http://' || address.substring(0,8) === 'https://';
+        }
+
+        return {
+            save: save,
+            load: load
         };
     });
--- www/js/types.js
+++ www/js/types.js
@@ -1,14 +1,26 @@
-angular.module('types', ['ionic'])
-    .controller('typeCtrl', function($scope, $http){
-       $scope.data = {};
+angular.module('types', [])
+    .controller('typeCtrl', function($scope, $http, configService){
 
-       $http.get('http://192.168.0.5:9000/types')
-           .success(function(data){
-              $scope.data.types = data;
-           })
-           .error(function(error){
-              $scope.data.error = error;
-           });
+        $scope.init = function(){
+            $scope.data = {};
+            var host = configService.load();
+
+            if(host !== '') {
+                console.error('>>>> Host: ' + host);
+                var promisse = $http({method: 'GET', url: host});
+
+                promisse.success(function(data){
+                    console.error('>>>>> Success');
+                    $scope.data.types = data;
+                }).error(function(error){
+                    console.error('Fail!');
+                    $scope.data.error = 'Error on connect to server.';
+                });
+            } else {
+                $scope.data.error = 'Error on connect to server.';
+            }
+        };
+
     })
     .directive('typesList', function(){
        return {
@@ -16,3 +28,16 @@ angular.module('types', ['ionic'])
           templateUrl: 'view/types/types-list.html'
        };
     });
+
+
+/*
+
+ $http.get('https://cors-test.appspot.com/test').then(function(resp) {
+ console.log('Success', resp);
+ // For JSON responses, resp.data contains the result
+ }, function(err) {
+ console.error('ERR', err);
+ // err.status will contain the status code
+ })
+
+*/
