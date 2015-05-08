f2ee12094836aefa828f321e361aa648bfc327e8
Anderson Zanichelli <anderzanic@gmail.com>
2015-05-08 00:01:57 -0300

    Adicionado comparação de passwords, adicionado biblioteca para criptografia

--- www/js/signup.js
+++ www/js/signup.js
@@ -1,6 +1,19 @@
 angular.module('signup', [])
     .controller('signupCtrl', function($scope, configService){
         $scope.host = configService.load();
+        $scope.didnotmatch = false;
+
+        $scope.confirmation = function(){
+            if($scope.password === $scope.confirm) {
+                $scope.didnotmatch = false;
+                $scope.signup();
+            } else
+                $scope.didnotmatch = true;
+        };
+
+        $scope.signup = function(){
+            console.log("fazer registro!");
+        };
     })
     .directive('signup', function(){
         return {
