66be9e650f1422669298188195045c66230df504
Anderson Zanichelli <anderzanic@gmail.com>
2015-07-04 00:37:22 -0300

    Criação da tela dinâmica de configurações

--- www/js/login.js
+++ www/js/login.js
@@ -3,24 +3,27 @@ angular.module('login', [])
         $scope.host = configService.load();
         $scope.message = '';
 
-        $scope.login = function(){
-            $scope.loading = true;
-            var user = {
-                "email": $scope.email,
-                "password": CryptoJS.SHA256($scope.password).toString()
-            };
-            var promisse = $http.get($scope.host + '/login/' + '/' + user['email'] + '/' + user['password']);
+        $scope.login = function() {
+            $scope.data.loggedIn = true;
+            if ($scope.email && $scope.password) {
+                $scope.loading = true;
+                var user = {
+                    "email": $scope.email,
+                    "password": CryptoJS.SHA256($scope.password).toString()
+                };
+                var promisse = $http.get($scope.host + '/login/' + '/' + user['email'] + '/' + user['password']);
 
-            promisse.success(function(data){
-                $scope.loading = false;
-                if(data.logged)
-                    $scope.data.loggedIn = true;
-                else
-                    $scope.message = 'User or password did not match';
-            }).error(function(error){
-                $scope.message = 'Error on login.';
-                $scope.loading = false;
-            });
+                promisse.success(function (data) {
+                    $scope.loading = false;
+                    if (data.logged)
+                        $scope.data.loggedIn = true;
+                    else
+                        $scope.message = 'User or password did not match';
+                }).error(function (error) {
+                    $scope.message = 'Error on login.';
+                    $scope.loading = false;
+                });
+            }
         }
     })
     .directive('login', function(){
--- www/js/preferences.js
+++ www/js/preferences.js
@@ -1,20 +1,68 @@
 angular.module('preferences', [])
-    .controller('preferencesCtrl', function($scope){
-
+    .controller('preferencesCtrl', function(){
     })
     .service('preferencesService', function(){
+        var data = {};
+
         var configuration = function(type, params){
-            console.log(type);
-            console.log(params);
+            data.type = type;
+            data.params = params;
         };
 
+        var getParams = function(){
+            return data.params;
+        }
+
         return {
-            configuration: configuration
+            configuration: configuration,
+            getParams: getParams
         };
     })
-    .directive('preferences', function(){
+    .directive('prefs', function(){
         return {
             restrict: 'E',
-            templateUrl: 'views/config/preferences.html'
+            templateUrl: 'view/config/prefs.html',
+            controller: function ($scope, preferencesService) {
+                $scope.create = function () {
+                    var div = $('<div>');
+                    $.each(preferencesService.getParams(), function (idx, obj) {
+                        var container = $('<div>');
+                        container.attr('id', 'container');
+
+                        var el = $('<' + obj.element + '>');
+                        el.attr('type', obj.type);
+                        el.attr('name', obj.name);
+
+                        if(obj.type === "number") {
+                            el.addClass("number");
+                        }
+
+                        var label = $('<label>');
+                        label.addClass('label');
+                        label.append(obj.label);
+
+                        if (obj.type === "checkbox") {
+                            container.append(el);
+                            container.append(label);
+                        } else {
+                            container.append(label);
+                            container.append(el);
+                        }
+
+                        if (obj.type === "select") {
+                            for (var item in obj.content) {
+                                var option = $('<option>');
+                                option.val(item);
+                                option.html(obj.content[item]);
+                                el.append(option);
+                            }
+                        }
+
+                        div.append(container);
+                    });
+
+                    $('#preferencia').html(div.html());
+                }
+            }
         };
     });
